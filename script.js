ac8acfe26ce35c133d35aeceff01974baeea3ea7
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-06-17 10:54:01 +0600

    second chapter

--- script.js
+++ script.js
@@ -28,9 +28,9 @@
 // let arr = [20, 34, 20, 45, 67, 89, 90, 100, 120, 130, 140, 150, 160, 170, 180, 190, 200];
 
 
-var arr = [20, 34, 20, 45, 67, 89, 90, 100, 120, 130, 140, 150, 160, 170, 180, 190, 200];
+// var arr = [20, 34, 20, 45, 67, 89, 90, 100, 120, 130, 140, 150, 160, 170, 180, 190, 200];
 
-var i=0;
+// var i=0;
 
 // for each loop 
 // arr.forEach(function(val){
@@ -40,38 +40,45 @@ var i=0;
 
 // map loop 
 
-var arr2= arr.map(function(val){
-    return val*2; 
-})
+// var arr2= arr.map(function(val){
+//     return val*2; 
+// })
 
 
-arr2.forEach(function(val){
-    console.log("-->>" + " The value is " + val);
-})
-// console.log(arr2);
+// arr2.forEach(function(val){
+//     console.log("-->>" + " The value is " + val);
+// })
+// // console.log(arr2);
 
-var newArr=arr2.filter(function(val){
-    return val%2==0;
-})
+// var newArr=arr2.filter(function(val){
+//     return val%2==0;
+// })
 
-console.log(newArr);
-
-
-var ans = arr2.find(function(val){
-    if(val==100){
-        return true;
-    }
-    else {
-        return false;
-    }
-}); 
-console.log(ans);
+// console.log(newArr);
 
 
+// var ans = arr2.find(function(val){
+//     if(val==100){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }); 
+// console.log(ans);
 
-function abcd(){
-    return 123; 
-}
 
 
-abcd(); 
+// function abcd(){
+//     return 123; 
+// }
+
+
+// abcd(); 
+
+
+var blob = await fetch(`https://randomuser.me/api/`); 
+
+var res = await blob.json();
+
+console.log(res);
