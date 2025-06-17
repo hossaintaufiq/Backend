cd50ea868d8e2820a6dcd5630b0a3195ec6d3f16
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-06-17 22:18:27 +0600

    server creating done

--- script.js
+++ script.js
@@ -1,48 +1,8 @@
-const fs = require('fs');
+const http= require('http');
 
 
-
-// Write "Hello world!" to a file named hello.txt
-
-// fs.writeFile("hello.txt", "Hello world!", function(err){
-//     if(err) console.error(err); 
-//     else console.log("File written successfully!");
-// })
-
-
-//  append something to the file hello.txt 
-
-// fs.appendFile("hello.txt", "\nNew line for the append operation", function(err){
-//     if(err) console.error(err); 
-//     else console.log("Append operation completed successfully!"); 
-// })
-
-// reading a file from the existing file system 
-// fs.readFile("hello.txt", function(err,data){
-//     if(err) console.error(err); 
-//     else console.log(data.toString()); 
-
-// })
-
-
-//  rename an existing file 
-// fs.rename("data.txt", "dataset.txt", function(err){
-//     if(err) console.error(err); 
-//     else console.log ("File renamed successfully to dataset.txt!");
-// })
-
-// coping a file to another location 
-
-// fs.copyFile("dataset.txt","Copy/dataset.txt", function(err){
-//     if(err) console.error(err); 
-//     else console.log("File copied successfully to Copy/dataset.txt!");
-// } )
-
-  
-
-// delettin file form the file system 
-
-fs.unlink("Copy/data.txt", function(err){
-    if(err) console.error(err); 
-    else console.log("File deleted successfully from Copy/data.txt!");
+const server= http.createServer(function(req, res ){
+    res.end("Hello world from the server!");
 })
+
+server.listen(3000); 
---
+++ script3.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+
+
+// Write "Hello world!" to a file named hello.txt
+
+// fs.writeFile("hello.txt", "Hello world!", function(err){
+//     if(err) console.error(err); 
+//     else console.log("File written successfully!");
+// })
+
+
+//  append something to the file hello.txt 
+
+// fs.appendFile("hello.txt", "\nNew line for the append operation", function(err){
+//     if(err) console.error(err); 
+//     else console.log("Append operation completed successfully!"); 
+// })
+
+// reading a file from the existing file system 
+// fs.readFile("hello.txt", function(err,data){
+//     if(err) console.error(err); 
+//     else console.log(data.toString()); 
+
+// })
+
+
+//  rename an existing file 
+// fs.rename("data.txt", "dataset.txt", function(err){
+//     if(err) console.error(err); 
+//     else console.log ("File renamed successfully to dataset.txt!");
+// })
+
+// coping a file to another location 
+
+// fs.copyFile("dataset.txt","Copy/dataset.txt", function(err){
+//     if(err) console.error(err); 
+//     else console.log("File copied successfully to Copy/dataset.txt!");
+// } )
+
+  
+
+// deleting file form the file system 
+
+// fs.unlink("Copy/data.txt", function(err){
+//     if(err) console.error(err); 
+//     else console.log("File deleted successfully from Copy/data.txt!");
+// })
+
+
