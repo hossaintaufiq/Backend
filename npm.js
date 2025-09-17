f8fb486f31902ecca12b60fd0f4fc9acd467ce55
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-09-17 11:47:59 +0600

    7 days mission

---
+++ npm.js
@@ -0,0 +1,23 @@
+// npm understanding 
+// installing and uninstalling packages 
+
+// install a package -> npm i pakages
+// uninstall a package -> npm uninstall package
+// installing particulaar version -> npm i package@version
+
+// devdependencies -> npm i package --save-dev (will use when developing not after that )
+
+
+// commands to learn 
+// npm start 
+// npm run dev
+// npm run build
+// npm run test
+
+
+// run comes when we decide a command else just direct command what is pre decided by the OS 
+
+
+
+
+console.log("npm understanding");
--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const http= require('http');
-
-
-const server= http.createServer(function(req, res ){
-    res.end("Hello world from the server!");
-})
-
-server.listen(3000); 
+console.log("npm understanding");
