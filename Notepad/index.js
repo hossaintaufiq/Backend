d945fee94e9e73eeb75505ada8f09fb20d152f8e
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-12-01 20:44:15 +0600

    notepad project

--- Notepad/index.js
+++ Notepad/index.js
@@ -1,13 +1,20 @@
 import express from 'express'
+import path from 'path'
 const app = express()
+// const path = require('path')
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+// app.use(express.static(path.join(__dirname,'/public')))
+app.use(express.static(path.join(process.cwd(), 'public')))
+app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.render('index')
 })
 
-app.listen(3000)
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+})
